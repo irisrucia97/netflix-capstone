0b732209ea2d58727326874af86e3da034cac615
irisrucia97 <irisrucia97@gmail.com>
2021-06-11 14:15:59 +0900

    Updates

---
+++ precache-manifest.109fffa2ff676b468f57a274d0475282.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9bbafedcc913395d010492670615a12d",
+    "url": "/netflix-capstone/index.html"
+  },
+  {
+    "revision": "7886358c27fea34f7f2b",
+    "url": "/netflix-capstone/static/css/2.764ccc25.chunk.css"
+  },
+  {
+    "revision": "7886358c27fea34f7f2b",
+    "url": "/netflix-capstone/static/js/2.11abb0c7.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/netflix-capstone/static/js/2.11abb0c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8a942fcaa8a2f40475e2",
+    "url": "/netflix-capstone/static/js/main.e6bb7bf9.chunk.js"
+  },
+  {
+    "revision": "4f8dee90f92edf533d22",
+    "url": "/netflix-capstone/static/js/runtime-main.67d66d22.js"
+  },
+  {
+    "revision": "6dbba458959d4ce1edd2f5b3ab3ae13b",
+    "url": "/netflix-capstone/static/media/logo.6dbba458.svg"
+  }
+]);
